9df91ae70f4ff0fe1acc89be8742978869ad0b7c
IwonaJarkiewicz <politea@gmail.com>
2017-08-27 22:34:16 +0200

    suma i iloczyn

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function suma(tablicaLiczb) {
+    var suma = 0;
+    tablicaLiczb.forEach(function (element, index) {
+        suma += element;
+    });
+
+    return suma;
+};
+
+function mnozenie(tablicaLiczb) {
+    var iloczyn = 1;
+    tablicaLiczb.forEach(
+        function (element, index) {
+            iloczyn *= element;
+        });
+    return iloczyn;
+}
+
+var tablica = [1, 2, 3, 4, 5, 6];
+console.log(suma(tablica));
+console.log(mnozenie(tablica));
